b844e46caa989f526eb9b514849eab4b1f065187
unknown <nanojrq@gmail.com>
2024-05-27 23:28:16 -0300

    Creacion de tablas relacionadas

--- src/backend/aulas/models.py
+++ src/backend/aulas/models.py
@@ -5,26 +5,41 @@ from django.db import models
 
 
 
-class carrera(models.Model):
+class Carrera(models.Model):
     nombre = models.CharField(max_length=128, null=False, blank= False)
 
 
-class profesor(models.Model):
+class Profesor(models.Model):
     nombre = models.CharField(max_length=128, null=False, blank= False)
     apellido = models.CharField(max_length=128, null=False, blank= False)
     mostrar = models.CharField(max_length=256, null=False, blank= False)
 
 
-class materia(models.Model):
+class Materia(models.Model):
     nombre = models.CharField(max_length=128, null=False, blank= False)
     cant_alumnos = models.IntegerField(default=5, null=False, blank= False)
-    id_carrera = models.ForeignKey(carrera, on_delete=models.CASCADE)
-    id_profesor = models.ForeignKey(profesor, on_delete=models.CASCADE) 
+    id_carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    id_profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE) 
 
 
-class aula(models.Model):
+class Aula(models.Model):
     descripcion = models.CharField(max_length=128, null=False, blank= False)
     ubicacion = models.CharField(max_length=128, null=False, blank= False)
     cant_proyector = models.IntegerField(default=0)
     aforo = models.IntegerField(default=0)
     es_climatizada = models.BooleanField(default=False)
+
+
+class Reserva_Aula(models.Model):
+    id_aula = models.ForeignKey(Aula, on_delete=models.CASCADE)
+    fh_desde = models.DateTimeField()
+    fh_hasta = models.DateTimeField() 
+    observacion = models.TextField(max_length=256, null=False, blank= False)
+
+
+
+class Horario_Materia(models.Model):
+    id_materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
+    id_reserva = models.ForeignKey(Reserva_Aula, on_delete=models.CASCADE)
+    fh_desde = models.DateTimeField()
+    fh_hasta = models.DateTimeField() 
